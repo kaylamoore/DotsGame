b7187acc7d6e616624f4016aac6f88946fd4b8b0
Kayla Moore <kaylamoo@usc.edu>
2015-08-24 15:38:12 -0700

    completes pseudo code

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-//Create event listener for cliking a side 
-//event listener changes color
-//take turns 
-//function to check if box is complete 
-//fill in box event listener 
+//1. take turns 
+	//a-declare variables "player1" "player2" "counter"
+
+
+//2.Create event listener for cliking a side 
+	//a.event listener changes color to black
+	//b.not able to be clicked twice 
+	//c. skip to step three 
+
+
+
+
+//3.function to check if box is complete 
+	//a. fill in box event listener 
+	//b. check if there is a winner (4)
+	//c. if box is complete, then player who just completed box gets another turn 
+
+//4 checkWinner
+	//check for tie 
+	//a.reset button 
+	//b.alert winner 
